dac4b25f7f3abcd4453f5c26bf305690be3233cc
oscarguerrerolopez <oscar.computer.guy@gmail.com>
2024-12-14 21:58:30 +0100

    add vercel and meta

--- src/shared/middlewares/logger.middleware.ts
+++ src/shared/middlewares/logger.middleware.ts
@@ -29,34 +29,34 @@ export class LoggerMiddleware {
 				return originalSend(body); // Call the original `send` method
 			};
 
-			if (req.path && !req.path.includes('meta')) {
-				logger.info('init request', {
+			// if (req.path && !req.path.includes('meta')) {
+			logger.info('init request', {
+				service: 'boilerplate',
+				file: 'logger.ts',
+				property: 'LoggerMiddleware',
+				code: req.code ?? 'noCode',
+				body: req.body,
+				headers: req.headers,
+				method: `Method: ${req.method}, path: ${req.path}, host:${req.hostname}`
+			});
+
+			const logRequestTime = (): void => {
+				const endTime = new Date().getTime();
+				const elapsedTime = endTime - startTime;
+
+				logger.info(`Request time: ${elapsedTime}ms`, {
 					service: 'boilerplate',
 					file: 'logger.ts',
 					property: 'LoggerMiddleware',
 					code: req.code ?? 'noCode',
-					body: req.body,
+					body: responseBody,
 					headers: req.headers,
 					method: `Method: ${req.method}, path: ${req.path}, host:${req.hostname}`
 				});
+			};
 
-				const logRequestTime = (): void => {
-					const endTime = new Date().getTime();
-					const elapsedTime = endTime - startTime;
-
-					logger.info(`Request time: ${elapsedTime}ms`, {
-						service: 'boilerplate',
-						file: 'logger.ts',
-						property: 'LoggerMiddleware',
-						code: req.code ?? 'noCode',
-						body: responseBody,
-						headers: req.headers,
-						method: `Method: ${req.method}, path: ${req.path}, host:${req.hostname}`
-					});
-				};
-
-				res.on('finish', logRequestTime);
-			}
+			res.on('finish', logRequestTime);
+			// }
 
 			next();
 		} catch (error) {
